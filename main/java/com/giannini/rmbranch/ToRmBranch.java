59e01c9d3f0f54852ca00d8511b68781e7eb11a1
Giannini <giannini@gmail.com>
2017-12-14 12:06:03 +0800

    to test rm branch

---
+++ main/java/com/giannini/rmbranch/ToRmBranch.java
@@ -0,0 +1,11 @@
+package com.giannini.rmbranch;
+
+/**
+ * @date 2017/12/14
+ */
+public class ToRmBranch {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
